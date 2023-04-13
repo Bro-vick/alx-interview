e498495f17468961773c87001ea3fefca6b666b9
george <georgev421@gmail.com>
2023-04-13 21:48:39 +0100

    Log parsing Solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,28 +15,52 @@ calculate
 10)if !key || !value, continue else print status code
 """
 
-import re
-import sys
+def parseLogs():
+    """
+    Reads logs from standard input and generates reports
+    Reports:
+        * Prints log size after reading every 10 lines & at KeyboardInterrupt
+    Raises:
+        KeyboardInterrupt (Exception): handles this exception and raises it
+    """
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
 
-sts_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 405: 0, 500: 0}
-stdin = sys.stdin
-counter = 0
-total_file_size = 0
-
-try:
-    for line in stdin:
-        output = line.split()
-        if int(output[-2]) in sts_dict.keys() and len(output) == 9:
-            counter += 1
-            sts_dict[int(output[-2])] += 1
-            total_file_size += int(output[-1])
-        if counter % 10 == 0:
-            print('File size: {}'.format(total_file_size))
-            for key, value in sts_dict.items():
-                if value:
-                    print("{}: {}".format(key, value))
-except KeyboardInterrupt as error:
-    print('File size: {}'.format(total_file_size))
-    for key, value in sts_dict.items():
+def report(fileSize, statusCodes):
+    """
+    This function Prints generated report to standard output
+    Args:
+        fileSize (int): This is the total log size after every 10 successfully read line
+        statusCodes (dict): The dictionary of status codes and counts
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
         print("{}: {}".format(key, value))
+
+
+if __name__ == '__main__':
+    parseLogs()

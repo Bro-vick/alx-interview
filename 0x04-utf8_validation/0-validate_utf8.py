19d80cf749e61ede3cb093a7d9adaf337bb4c03f
george <georgev421@gmail.com>
2023-04-21 00:41:30 +0100

    utf8 validation interviw prep

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""
+A method that determines if a given data set represents a valid
+UTF-8 encoding.
+"""
+
 def validUTF8(data):
     # Loop through each byte in the data list
     i = 0
